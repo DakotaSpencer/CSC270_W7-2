5edf8820ab1a52750ce47263c927f44ad6c460eb
ArturoDuran90 <aduran@student.neumont.edu>
2023-08-25 16:58:34 -0600

    added cart

--- w7-2/w7-2/Cart.php
+++ w7-2/w7-2/Cart.php
@@ -1,42 +1,57 @@
 <?php
-use CommonMark\Node\Image;
+require "dbConnector.php";
 include_once "Header.php";
-?>
-<!--<p>&#x1F6D2;</p>-->
-<div>
-    <h2>Cart</h2>
-    <table>
-        <tr>
-            <th>Image</th>
-            <th>Item Name</th>
-            <th>Price</th>
-            <th>Delete</th>
-        </tr>
-        <tr>
-            <?php
-            $product = ""; //Need to get the id from products page. products page => add to cart, product_id goes here.
-            $url = 'https://fakestoreapi.com/products/1'; //${product_Id}';
 
-            $ch = curl_init($url);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            $response = curl_exec($ch);
-            $response = json_decode($response);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve the form data
+    $id = strval($_GET['productId']);
 
-            //image
-            //name
-            //price
-            echo "
-                <td>{$response}</td> 
-                <td>{$response}</td>
-                <td>{$response}</td>
-            ";
-            curl_close($ch);
-            ?>
-            <td>&#x2716;</td>
-        </tr>
-    </table>
-</div>
+    // Create the database connection
+    $dbConn = ConmnGet();
 
-<?php
+    // Call the Read function to fetch the JSON data.
+    $jsonData = GetProductFromCart($dbConn, $id);
+
+    // Check if the data was retrieved successfully.
+    if ($jsonData) {
+        // Fetch the JSON object from the result set.
+        $row = mysqli_fetch_assoc($jsonData);
+
+        // Decode the JSON data into an associative array.
+        $productInfo = json_decode($row['Json1'], true);
+
+        // Check if the JSON was successfully decoded.
+        if ($productInfo) {
+        // Create a table to display all vehicle information
+        echo '
+            <div>
+                <h2>Cart</h2>
+                <table>
+                    <tr>
+                        <th>Image</th>
+                        <th>Item Name</th>
+                        <th>Price</th>
+                        <th>Delete</th>
+                    </tr>
+                    <tr>
+                        <td>' . $productInfo["image"] . '</td>
+                        <td>' . $productInfo["title"] . '</td>
+                        <td>' . $productInfo["price"] . '</td>
+                        <td>&#x2716;</td>
+                    </tr>
+                </table>
+            </div>
+        ';
+
+    } else {
+        echo "<h1>Failed to decode vehicle information. Please try again.</h1>";
+    }
+} else {
+    echo "<h1>Failed to read vehicle. Please try again.</h1>";
+}
+
+// Close the database connection
+mysqli_close($dbConn);
+
 include_once "Footer.php";
 ?>
